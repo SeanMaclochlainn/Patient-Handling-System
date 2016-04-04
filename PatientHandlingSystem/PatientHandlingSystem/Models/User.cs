e8ce4f83a3044daf01f619d929bbff17a6601601
SeanMaclochlainn <seanmcloughlin9@gmail.com>
2016-04-04 15:45:55 +0100

    Added correct display names

--- PatientHandlingSystem/PatientHandlingSystem/Models/User.cs
+++ PatientHandlingSystem/PatientHandlingSystem/Models/User.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace PatientHandlingSystem.ViewModels
+namespace PatientHandlingSystem.Models
 {
-    public class UserVM
+    public class User
     {
         public int ID { get; set; }
 
@@ -19,12 +18,5 @@ namespace PatientHandlingSystem.ViewModels
 
         [DisplayName("E-mail Address")]
         public string EmailAddress { get; set; }
-
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-        public List<string> Roles { get; set; } //to store role drop down elements
-
-        [DisplayName("Role")]
-        public string SelectedRole { get; set; } // to store selected role from drop down
     }
 }
